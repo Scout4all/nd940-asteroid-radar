45a5fcf6aca2c562e810271828ddddfcf2073760
Bigad Aboubakr <scout4all@gmail.com>
2023-01-21 21:48:51 +0200

    update gradle and files

---
+++ app/src/main/java/me/bigad/asteroidradar/MainActivity.kt
@@ -0,0 +1,14 @@
+package me.bigad.asteroidradar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ app/src/main/java/me/bigad/asteroidradar/screens/main/MainViewModel.kt
@@ -0,0 +1,6 @@
+package me.bigad.asteroidradar.screens.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+}
